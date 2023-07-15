46be889f5dad52d816a9323f2f656398886b167e
Calvin Costa <costacalvin0@gmail.com>
2023-07-15 19:34:53 -0400

    made some edits to hide auth for grafbase

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -34,15 +34,15 @@ const Project = g
     rules.private().create().delete().update();
   });
 
-const jwt = auth.JWT({
-  issuer: "grafbase",
-  secret: g.env("NEXTAUTH_SECRET"),
-});
+// const jwt = auth.JWT({
+//   issuer: "grafbase",
+//   secret: g.env("NEXTAUTH_SECRET"),
+// });
 
 export default config({
   schema: g,
-  auth: {
-    providers: [jwt],
-    rules: (rules) => rules.private(),
-  },
+  // auth: {
+  //   providers: [jwt],
+  //   rules: (rules) => rules.private(),
+  // },
 });
