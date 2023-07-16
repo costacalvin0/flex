fab117185517d9723bc50a5364fb50c90abdde23
Calvin Costa <costacalvin0@gmail.com>
2023-07-16 00:43:39 -0400

    created auth using JWT and google

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button key={i} onClick={() => signIn(provider?.id)}>
+            {provider.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
