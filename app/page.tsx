5a5f456c8c62e59a783aa40511514913b2cbe814
Calvin Costa <costacalvin0@gmail.com>
2023-07-16 22:09:37 -0400

    added post rendering to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
-const Home = () => {
+import { ProjectInterface } from "@/common.types";
+import ProjectCard from "@/components/ProjectCard";
+import { fetchAllProjects } from "@/lib/actions";
+
+type ProjectSearch = {
+  projectSearch: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  };
+};
+const Home = async () => {
+  const data = (await fetchAllProjects()) as ProjectSearch;
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+        Categories
+        <p className="no-result-text text-center">
+          No projects found, go create some first.
+        </p>
+      </section>
+    );
+  }
   return (
     <section className="flex-start flex-col paddings mb-16">
       <h1>Categories</h1>
-      <h1>Posts</h1>
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+      </section>
       <h1>LoadMore</h1>
     </section>
   );
